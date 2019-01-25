117ed80e1e364e3e103eb46ad52540bd54e19f4c
github username <github email address>
2019-01-25 12:01:04 -0500

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,6 +1,8 @@
 def snake_it_up(string)
   if string[0] == "s"
-  10 * "s" + string
+    answer = 10.times do string.insert(0, "s")
+  end
+  return string
   else
   string
   end
--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -12,7 +12,9 @@ def crew
 end
 
 def greet_crew (crew)
-  crew.each {|crew_member| "Hello #{crew_member}."}
+  crew.map do |crew_member|
+    "Hello #{crew_member}."
+  end
 end
 
 def engage
